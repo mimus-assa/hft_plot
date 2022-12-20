f654ce8b16cfe0bb8b6fa907908203988a696c94
mimus-assa <chavezgm2012@gmail.com>
2022-12-19 22:27:30 -0600

    make the ploty js code a module named hft_ploty.js

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
 import os
+import json
 import pandas as pd
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
-# Retrieve the list of files in the datas folder
 files = os.listdir('datas')
-
 @app.route('/', methods=['GET'])
 def index():
-    """Render the index.html template with the list of files"""
     return render_template('index.html', files=files)
-
 @app.route('/hft_plot', methods=['POST'])
 def hft_plot():
-    """Render the plot.html template with the data from the selected file"""
     filename = request.form['file']
     df = pd.read_csv('datas/' + filename)
     # Select only the columns of interest
     df = df[['datetime', 'delta']]
     return render_template('hft_plot.html', df=df.to_dict(), files=files, filename=filename)
-
 @app.route('/savetimestamps', methods=['POST'])
 def save_timestamps():
-    """Save the timestamps in a csv file"""
     timestamps = request.form['timestamps']
     filename = request.form['filename']
     # Convert the timestamps from json to a list
@@ -32,7 +24,5 @@ def save_timestamps():
     # Save the data in a csv
     df.to_csv(filename, index=False)
     return jsonify({"message": "saved"})
-
 if __name__ == '__main__':
-    # Run the application in debug mode
     app.run(debug=True)
---
+++ static/hft_ploty.js
@@ -0,0 +1,36 @@
+// This function is used to create the plotly plot from the data
+function createPlot(data) {
+    var trace = {
+        x: Object.values(data.datetime),
+        y: Object.values(data.delta),
+        type: 'scatter'
+    };
+    var layout = {
+        xaxis: {
+            range: [0, 500]
+        },
+        margin: {
+            l: 40,
+            r: 40,
+            b: 40,
+            t: 40
+        },
+        dragmode: 'pan'
+    };
+    Plotly.newPlot('plotly', [trace], layout);
+    document.getElementById('plotly').on('plotly_click', function(data){
+        var x = data.points[0].x;
+        var y = data.points[0].y;
+        timestamps.push(x);
+        var newTrace = {
+            x: [x],
+            y: [y],
+            type: 'scatter',
+            mode: 'markers',
+            marker: {
+                color: 'red'
+            }
+        };
+        Plotly.addTraces('plotly', newTrace);
+    });
+}
