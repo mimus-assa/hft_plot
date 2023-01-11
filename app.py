c0014019b1c54b18c28d9c0f5ebd2a0bafcad5d8
mimus-assa <chavezgm2012@gmail.com>
2023-01-10 23:29:44 -0600

    add a new save route for the two plots

--- app.py
+++ app.py
@@ -25,27 +25,29 @@ def btc_trades_analysis():
     df = df[['datetime', 'open', 'high', 'low', 'close']]
     return render_template('btc_trades_analysis.html', df=df.to_dict(), files=files, filename=filename)
 
-
 @app.route('/savetimestamps', methods=['POST'])
 def save_timestamps():
-    
     data = request.form['timestamps']
     filename = request.form['filename']
     data = json.loads(data)
-    print(data)
-        # create an empty dictionary
-    new_dict = {"x":[],"y":[],"type":[],"mode":[],"marker":[],"name":[]}
-
+    # create an empty dictionary
+    new_dict = {"x":[],"marker":[]}
     # loop through your_list
     for item in data:
         new_dict["x"].append(item["x"][0]) #x value is a list, take the first element
-        new_dict["y"].append(item["y"][0]) #y value is a list, take the first element
-        new_dict["type"].append(item["type"])
-        new_dict["mode"].append(item["mode"])
         new_dict["marker"].append(item["marker"]["color"])
-        new_dict["name"].append(item["name"])
-    print(new_dict)   
     df = pd.DataFrame({"timestamps": new_dict["x"],"color": new_dict["marker"]})
+    # Save the data in a csv
+    df.to_csv(filename, index=False)
+    return jsonify({"message": "saved"})
+
+@app.route('/savetimestamps2', methods=['POST'])
+def save_timestamps2():
+    timestamps = request.form['timestamps']
+    filename = request.form['filename']
+    # Convert the timestamps from json to a list
+    timestamps = json.loads(timestamps)
+    df = pd.DataFrame({"timestamps": timestamps})
     # Save the data in a csv
     df.to_csv(filename, index=False)
     return jsonify({"message": "saved"})
