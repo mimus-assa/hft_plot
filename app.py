1ca247968c619f155512d7867db5789b011c56c8
mimus-assa <chavezgm2012@gmail.com>
2023-01-12 00:12:37 -0600

    working whit 1s and 5m data

--- app.py
+++ app.py
@@ -3,27 +3,28 @@ import json
 import pandas as pd
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-files = os.listdir('datas')
+files_1s = os.listdir('datas/1s/')
+files_5m = os.listdir('datas/5m/')
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', files=files)
+    return render_template('index.html', files1=files_1s, files2=files_5m)
 
 @app.route('/hft_plot', methods=['POST','GET'])
 def hft_plot():
     filename = request.form['file']
-    df = pd.read_csv('datas/' + filename)
+    df = pd.read_csv('datas/1s/' + filename)
     # Select only the columns of interest
     df = df[['datetime', 'delta']]
-    return render_template('hft_plot.html', df=df.to_dict(), files=files, filename=filename)
+    return render_template('hft_plot.html', df=df.to_dict(), files=files_1s, filename=filename)
 
 @app.route('/btc_trades_analysis', methods=['POST','GET'])
 def btc_trades_analysis():
     filename = request.form['file']
-    df = pd.read_csv('datas/' + filename)
+    df = pd.read_csv('datas/5m/' + filename)
     # Select only the columns of interest
     df = df[['datetime', 'open', 'high', 'low', 'close']]
-    return render_template('btc_trades_analysis.html', df=df.to_dict(), files=files, filename=filename)
+    return render_template('btc_trades_analysis.html', df=df.to_dict(), files=files_5m, filename=filename)
 
 @app.route('/savetimestamps', methods=['POST'])
 def save_timestamps():
